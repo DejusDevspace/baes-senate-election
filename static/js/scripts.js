32e853ec213768e53047427139943fa1348303bd
Deju Adejo <ojomideju2003@gmail.com>
2024-12-06 07:26:44 +0100

    updated radio toggle to checkbox

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,16 +1,23 @@
-function toggleSelection(radio) {
-  const radios = document.querySelectorAll(
-    'input[type="radio"][name="candidate"]'
+function toggleSelection(checkbox, groupName) {
+  // Select all checkboxes with the same name as the group
+  const checkboxes = document.querySelectorAll(
+    `input[type="checkbox"][name="${groupName}"]`
   );
-  radios.forEach((r) => {
-    if (r !== radio) r.checked = false;
+
+  // Deselect other checkboxes in the group
+  checkboxes.forEach((cb) => {
+    if (cb !== checkbox) {
+      cb.checked = false;
+      cb.dataset.checked = "false";
+    }
   });
-  // Allow deselecting the same radio
-  if (radio.dataset.checked === "true") {
-    radio.checked = false;
-    radio.dataset.checked = "false";
+
+  // Allow deselecting the same checkbox
+  if (checkbox.dataset.checked === "true") {
+    checkbox.checked = false;
+    checkbox.dataset.checked = "false";
   } else {
-    radio.dataset.checked = "true";
+    checkbox.dataset.checked = "true";
   }
 }
 
@@ -27,7 +34,7 @@ document
 
     if (!headSelected || !chairmanSelected) {
       // Show an alert if the user hasn't selected all required options
-      alert("Please select a candidate for both positions before submitting.");
+      alert("Please select a candidate from each position before submitting.");
     } else {
       // If both are selected, show the confirmation modal
       const confirmationModal = new bootstrap.Modal(
@@ -41,3 +48,42 @@ document.getElementById("confirmSubmit").addEventListener("click", function () {
   // Submit the form if the user confirms
   document.getElementById("voteForm").submit();
 });
+
+// Custom Alert
+// function CustomAlert() {
+//   this.alert = function (message, title) {
+//     document.body.innerHTML =
+//       document.body.innerHTML +
+//       '<div id="dialogoverlay"></div><div id="dialogbox" class="slit-in-vertical"><div><div id="dialogboxhead"></div><div id="dialogboxbody"></div><div id="dialogboxfoot"></div></div></div>';
+
+//     let dialogoverlay = document.getElementById("dialogoverlay");
+//     let dialogbox = document.getElementById("dialogbox");
+
+//     let winH = window.innerHeight;
+//     dialogoverlay.style.height = winH + "px";
+
+//     dialogbox.style.top = "100px";
+
+//     dialogoverlay.style.display = "block";
+//     dialogbox.style.display = "block";
+
+//     document.getElementById("dialogboxhead").style.display = "block";
+
+//     if (typeof title === "undefined") {
+//       document.getElementById("dialogboxhead").style.display = "none";
+//     } else {
+//       document.getElementById("dialogboxhead").innerHTML =
+//         '<i class="fa fa-exclamation-circle" aria-hidden="true"></i> ' + title;
+//     }
+//     document.getElementById("dialogboxbody").innerHTML = message;
+//     document.getElementById("dialogboxfoot").innerHTML =
+//       '<button class="pure-material-button-contained active" onclick="customAlert.ok()">OK</button>';
+//   };
+
+//   this.ok = function () {
+//     document.getElementById("dialogbox").style.display = "none";
+//     document.getElementById("dialogoverlay").style.display = "none";
+//   };
+// }
+
+// let customAlert = new CustomAlert();
